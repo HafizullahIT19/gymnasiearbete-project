c05fd59b1f8ee961217bb64bbe004340afd988b7
HafizullahIT19 <tisudd19hafrah@gmail.com>
2021-11-24 10:51:24 +0100

    Popular-container and articles Styles add

--- src/index.js
+++ src/index.js
@@ -12,6 +12,13 @@ import './scss/main.scss'
 import './images/coding.png'
 import './images/logo.png'
 import './images/banner2.png'
+import './images/1.png'
+import './images/2.png'
+import './images/3.png'
+import './images/4.png'
+import './images/5.png'
+import './images/6.png'
+
 
 
 //JS Files
